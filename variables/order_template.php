2d2ef4c56e7de9dbbb94736a9d0e5e3743f54cc2
Noah Ramey <noahramey1997@gmail.com>
2016-12-10 20:50:24 -0800

    add more variable practice

--- variables/order_template.php
+++ variables/order_template.php
@@ -3,9 +3,10 @@
     $downtown_address = "208 SW 5th Ave #105, Portland, OR 97204";
     $northwest_address = "1978 19th Ave, Portland, OR 97209";
     $south_address = "1493 SW 2nd Ave, Portland, OR 97201";
-    $downtown_order = "300 Cups";
-    $northwest_order = "500 Cups";
-    $south_order = "1000 Cups";
+    $east_address = "9019 SW 15th Ave, Portland, OR 97219";
+    $downtown_order = "Milk and Cream";
+    $northwest_order = "Cream";
+    $south_order = "Milk";
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +14,7 @@
   <head>
     <meta charset="utf-8">
     <title>Order Form</title>
+    <link rel="stylesheet" href="coffee_styles.css">
   </head>
   <body>
     <p>Hi <?php echo $recipient; ?>, </p>
